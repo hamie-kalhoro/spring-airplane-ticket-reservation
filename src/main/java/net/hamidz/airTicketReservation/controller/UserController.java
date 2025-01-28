a13b56c3790b6a5e8822cba92aa0f98fd0204c71
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-01-28 13:59:03 +0500

    adds getUser by id and deleteUser by id calls

--- src/main/java/net/hamidz/airTicketReservation/controller/UserController.java
+++ src/main/java/net/hamidz/airTicketReservation/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -25,5 +26,16 @@ public class UserController {
     @GetMapping("/get-all-users")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.findUserById(id), HttpStatus.FOUND);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/net/hamidz/airTicketReservation/service/UserService.java
+++ src/main/java/net/hamidz/airTicketReservation/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,6 +23,21 @@ public class UserService {
 
     public List<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> findUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found!!!"));
+        return Optional.of(user);
+    }
+
+    public void deleteUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found :-("));
+        if(user != null && !user.equals("")) {
+            userRepository.delete(user);
+        } else {
+            throw new RuntimeException("invalid user type");
+        }
     }
 
 }
