4c7a7404cb651a772cc95bca2d813feb6de93125
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-01-28 15:08:57 +0500

    adding table to mysql and pojo is created!

---
+++ src/main/java/net/hamidz/airTicketReservation/entity/Ticket.java
@@ -0,0 +1,21 @@
+package net.hamidz.airTicketReservation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ticketType;
+    private String ticketNumber;
+    private LocalDateTime ticketAllocationDate;
+}
